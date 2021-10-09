c07e71957f66f8cdfb40cca5acbfce891bde6741
Harikeshav R <r.harikeshav@gmail.com>
2021-10-09 16:13:21 +0530

    Tried Adding Mask (Not Working)

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -7,21 +7,112 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+from bs4 import BeautifulSoup
+import requests
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class ActionShowMasks(Action):
+
+    def name(self) -> Text:
+        return "action_show_masks"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        def get_title(soup):
+            try:
+                title = soup.find("span", attrs={"id": 'productTitle'})
+                title_value = title.string
+                title_string = title_value.strip()
+
+            except AttributeError:
+                title_string = ""
+
+            return title_string
+
+        def get_price(soup):
+            try:
+                price = soup.find("span", attrs={'id': 'priceblock_ourprice'}).string.strip()
+
+            except AttributeError:
+                try:
+                    price = soup.find("span", attrs={'id': 'priceblock_dealprice'}).string.strip()
+
+                except:
+                    price = ""
+
+            return price
+
+        def get_rating(soup):
+            try:
+                rating = soup.find("i", attrs={'class': 'a-icon a-icon-star a-star-4-5'}).string.strip()
+            except AttributeError:
+                try:
+                    rating = soup.find("span", attrs={'class': 'a-icon-alt'}).string.strip()
+                except:
+                    rating = ""
+
+            return rating
+
+        def get_review_count(soup):
+            try:
+                review_count = soup.find("span", attrs={'id': 'acrCustomerReviewText'}).string.strip()
+
+            except AttributeError:
+                review_count = ""
+
+            return review_count
+
+        def get_availability(soup):
+            try:
+                available = soup.find("div", attrs={'id': 'availability'})
+                available = available.find("span").string.strip()
+
+            except AttributeError:
+                available = "Not Available"
+
+            return available
+
+        HEADERS = ({'User-Agent':
+                        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+                    'Accept-Language': 'en-US'})
+
+        URL = "https://www.amazon.in/s?k=masks&i=hpc&ref=nb_sb_noss_1"
+        webpage = requests.get(URL, headers=HEADERS)
+        soup = BeautifulSoup(webpage.content, "lxml")
+        links = soup.find_all("a", attrs={'class': 'a-link-normal s-no-outline'})
+        links_list = []
+
+        for link in links:
+            links_list.append(link.get('href'))
+
+        dispatcher.utter_message(text="Check these deals out:")
+
+        i = 0
+
+        for link in links_list:
+            dispatcher.utter_message(text=("https://amazon.in/" + link))
+            i += 1
+            if i == 5:
+                break
+
+        return []
