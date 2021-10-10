06386b120ed5c51f886ddbb2841eadfd88beeb52
Harikeshav R <r.harikeshav@gmail.com>
2021-10-10 13:48:29 +0530

    chatbot done

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -10,7 +10,6 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-from bs4 import BeautifulSoup
 import requests
 
 import json
